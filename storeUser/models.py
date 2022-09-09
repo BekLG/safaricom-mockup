860a878fa5d494c910f1c00193a743b974801ba0
Yab <yabuman46@gmail.com>
2022-09-09 18:31:17 +0300

    Modified the models.py file

--- storeUser/models.py
+++ storeUser/models.py
@@ -4,13 +4,13 @@ from django.contrib.auth.models import User
 
 class userProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    fname = models.CharField(label='First Name',max_length=50)
-    lname = models.CharField(label='Last Name',max_length=50)
-    educationLevel = models.CharField(label='Education Level',max_length=50)
-    age = models.IntegerField(label='Age')
-    sex = models.CharField(label='Gender',max_length=10)
-    phoneNumber = models.CharField(label='Phonenumber',max_length=20)
-    address = models.CharField(label='Address',max_length=100)
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    educationLevel = models.CharField(max_length=50)
+    age = models.IntegerField(default=0)
+    sex = models.CharField(max_length=10)
+    phoneNumber = models.CharField(max_length=20)
+    address = models.CharField(max_length=100)
 
     def save(self, *args,**kwargs):
         super().save(*args,**kwargs)
