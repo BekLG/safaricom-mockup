ed2a80d580c9817109b38df465b75fea1f08c5db
Yab <yabuman46@gmail.com>
2022-09-10 00:16:09 +0300

    Removed the debug is true

--- safaricom_mockup/settings.py
+++ safaricom_mockup/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vn0yep#&#hu()8pv(qw%e$(h8xa#f1wa5v+b0%n7upbo5lpl0z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['safaricom-gift.herokuapp.com']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
